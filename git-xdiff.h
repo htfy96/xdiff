0e6608be7e9c279b3a1056633fcfb22fca68aaf8
Vic Luo <vicluo96@gmail.com>
2023-06-15 22:38:09 -0400

    git-xdiff.h: fix inline definition to follow C11

--- git-xdiff.h
+++ git-xdiff.h
@@ -39,7 +39,7 @@
 #define xdl_regex_t regex_t
 #define xdl_regmatch_t regmatch_t
 
-inline int xdl_regexec_buf(
+static inline int xdl_regexec_buf(
 	const xdl_regex_t *preg, const char *buf, size_t size,
 	size_t nmatch, xdl_regmatch_t pmatch[], int eflags)
 {
